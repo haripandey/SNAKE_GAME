a5e6d0e81bcf76307ba581921a6888bdbdfe9b32
Hari <haripandey@users.noreply.github.com>
2015-03-29 19:35:50 -0700

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "hari.h"
+#include "paarth.h"
+
+using std::cout;
+using std::endl;
+
+int main(int argc, char ** argv) {
+	cout << "Hari Pandey's First Game C++ Homework" << endl;
+	GameplayDirector::gdStart(argc, argv);
+}
